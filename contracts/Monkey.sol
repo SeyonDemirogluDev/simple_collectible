1cbe06b06cc2e8debb04bdc86a887029b3f45ce3
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-11 11:41:46 +0200

    Update contract

--- contracts/Monkey.sol
+++ contracts/Monkey.sol
@@ -3,7 +3,21 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract Monkey is ERC721("Monkey", "MONKEY") {
-    
+contract Monkey is ERC721("Monkey", "MONKEY"), Pausable, Ownable, ERC721Burnable {
+
+    function mint(address to, uint256 tokenId) external whenNotPaused {
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
 }
