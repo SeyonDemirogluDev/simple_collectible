56139171f6fff9f205ce5c6bbd55d96b42eaa410
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-20 23:50:52 +0200

    Remove parameter

--- contracts/Monkey.sol
+++ contracts/Monkey.sol
@@ -3,13 +3,13 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract Monkey is ERC721("Monkey", "MONKEY"), Pausable, Ownable, ERC721Burnable {
+contract Monkey is ERC721("Monkey", "MONKE"), ERC721Burnable, Pausable, Ownable {
 
-    function mint(address to, uint256 tokenId) external whenNotPaused {
+    function mint(uint256 tokenId) external whenNotPaused {
         _safeMint(msg.sender, tokenId);
     }
 
